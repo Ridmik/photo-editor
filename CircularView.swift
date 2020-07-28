887ead2bed1e5b0ddca924d3564020f924c551df
Mufakkharul Islam Nayem <nil.nayem@gmail.com>
2020-07-28 16:12:35 +0600

    Add opaque circular shape to icon buttons
    - Added @IBDesignable CircularView class
    - Used CircularView as the background view for icon buttons in PhotoEditorViewController
    - Used 30% black as the background color of all the CircularViews

---
+++ CircularView.swift
@@ -0,0 +1,48 @@
+//
+//  CircularView.swift
+//  iOSPhotoEditor
+//
+//  Created by Mufakkharul Islam Nayem on 28/7/20.
+//
+
+import UIKit
+
+@IBDesignable class CircularView: UIView {
+    
+    private var _round = true
+    
+    @IBInspectable var circular: Bool {
+        set {
+            _round = newValue
+            makeRound()
+        }
+        get {
+            return self._round
+        }
+    }
+    
+    override internal var frame: CGRect {
+        set {
+            super.frame = newValue
+            makeRound()
+        }
+        get {
+            return super.frame
+        }
+        
+    }
+    
+    private func makeRound() {
+        if self.circular == true {
+            self.clipsToBounds = true
+            self.layer.cornerRadius = (self.frame.width + self.frame.height) / 4
+        } else {
+            self.layer.cornerRadius = 0
+        }
+    }
+    
+    override func layoutSubviews() {
+        makeRound()
+    }
+    
+}
