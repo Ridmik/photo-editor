fc2dd117064a057544d6809e5aedea7cad97a618
Mufakkharul Islam Nayem <nil.nayem@gmail.com>
2020-08-05 16:14:13 +0600

    Unlock video picking with UIImagePickerController in example project

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
         let picker = UIImagePickerController()
         picker.delegate = self
         picker.sourceType = .photoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
         present(picker, animated: true, completion: nil)
     }
 }
@@ -38,47 +39,33 @@ extension ViewController: PhotoEditorDelegate {
 
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    func imagePickerController(_ picker: UIImagePickerController,
-                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-// Local variable inserted by Swift 4.2 migrator.
-let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
-        guard let image = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as? UIImage else {
-            picker.dismiss(animated: true, completion: nil)
-            return
-        }
         picker.dismiss(animated: true, completion: nil)
         
-        
-        let photoEditor = PhotoEditorViewController(nibName:"PhotoEditorViewController",bundle: Bundle(for: PhotoEditorViewController.self))
-        photoEditor.photoEditorDelegate = self
-        photoEditor.image = image
-        //Colors for drawing and Text, If not set default values will be used
-        //photoEditor.colors = [.red, .blue, .green]
-        
-        //Stickers that the user will choose from to add on the image
-        for i in 0...10 {
-            photoEditor.stickers.append(UIImage(named: i.description )!)
+        if let image = info[.originalImage] as? UIImage {
+            let photoEditor = PhotoEditorViewController(nibName:"PhotoEditorViewController",bundle: Bundle(for: PhotoEditorViewController.self))
+            photoEditor.photoEditorDelegate = self
+            photoEditor.image = image
+            //Colors for drawing and Text, If not set default values will be used
+            //photoEditor.colors = [.red, .blue, .green]
+            
+            //Stickers that the user will choose from to add on the image
+            for i in 0...10 {
+                photoEditor.stickers.append(UIImage(named: i.description )!)
+            }
+            
+            //To hide controls - array of enum Control
+            //photoEditor.hiddenControls = [.crop, .draw, .share]
+            photoEditor.modalPresentationStyle = UIModalPresentationStyle.currentContext //or .overFullScreen for transparency
+            present(photoEditor, animated: true, completion: nil)
+        } else if let videoURL = info[.mediaURL] as? URL {
+            print("Handle video located at: \(videoURL)")
         }
         
-        //To hide controls - array of enum Control
-        //photoEditor.hiddenControls = [.crop, .draw, .share]
-        photoEditor.modalPresentationStyle = UIModalPresentationStyle.currentContext //or .overFullScreen for transparency
-        present(photoEditor, animated: true, completion: nil)
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
-}
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
-	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
-}
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
-	return input.rawValue
 }
